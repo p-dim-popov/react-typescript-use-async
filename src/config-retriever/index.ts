d56ccf6a27cd2e2bcdd0464c05d34f8d5c950810
Peter Popov <p.dim.popov@gmail.com>
2022-12-27 22:23:53 +0200

    chore(temp): temp

--- src/config-retriever/index.ts
+++ src/config-retriever/index.ts
@@ -1,74 +1,85 @@
-type ConfigValueDefinition<T> =
-  | StaticConfigValueDefinition<T>
-  | DynamicConfigValueDefinition<T>
-  | HybridConfigValueDefinition<T>
+import type { Notifier } from '../notifier'
+import { createNotifier } from '../notifier'
 
-type StaticConfigValueDefinition<T> = {
+type ConfigValueDefinition<K extends string, T> =
+  | StaticConfigValueDefinition<K, T>
+  | DynamicConfigValueDefinition<K, T>
+  | HybridConfigValueDefinition<K, T>
+
+type BaseConfigValueDefinition<Key extends string> = {
+  key: Key
+}
+
+type StaticConfigValueDefinition<
+  K extends string,
+  T
+> = BaseConfigValueDefinition<K> & {
   value: T
 }
 
-type DynamicConfigValueDefinition<T> = {
+type DynamicConfigValueDefinition<
+  K extends string,
+  T
+> = BaseConfigValueDefinition<K> & {
   retrieve: (key: string, abortController: AbortController) => Promise<T>
 }
 
-type HybridConfigValueDefinition<T> = StaticConfigValueDefinition<T> &
-  DynamicConfigValueDefinition<T>
+type HybridConfigValueDefinition<
+  K extends string,
+  T
+> = StaticConfigValueDefinition<K, T> & DynamicConfigValueDefinition<K, T>
 
 type StringKeys<T> = keyof T & string
 
 type ConfigValueRetriever<Definitions extends ExtendableOnlyDefinitions> = {
-  get: <Key extends StaticOnlyKey<Definitions>>(
+  get: <Key extends StaticOnlyKey<Definitions> & string>(
     key: Key
-  ) => Definitions[Key] extends ConfigValueDefinition<infer T> ? T : never
+  ) => Definitions[Key] extends ConfigValueDefinition<Key, infer T> ? T : never
   fetch: <Key extends StringKeys<Definitions>>(
     key: Key,
     controller?: AbortController
   ) => Promise<
-    Definitions[Key] extends ConfigValueDefinition<infer T> ? T : never
+    Definitions[Key] extends ConfigValueDefinition<Key, infer T> ? T : never
   >
   subscribe: Subscribe<Definitions>
 }
 
-type Subscribe<Definitions extends ExtendableOnlyDefinitions> = <
-  Key extends StringKeys<Definitions>,
-  A extends Action<any>
->(
-  key: Key,
-  type: A['type'],
-  handler: (payload: ReturnType<A['register']>) => void
-) => () => void
-
-type Dispatch<Definitions extends ExtendableOnlyDefinitions> = <
-  Key extends StringKeys<Definitions>,
-  A extends Action<Definitions>
->(
-  key: Key,
-  type: A['type'],
-  payload: ReturnType<A['register']>
-) => void
-
-type Action<Definitions extends ExtendableOnlyDefinitions> = {
-  type: 'value.change'
-  register: <Key extends DynamicOnlyKey<Definitions>>(
-    key: Key
-  ) => Definitions[Key]
+type Subscribe<Definitions extends ExtendableOnlyDefinitions> = {
+  onChange: <
+    Key extends string & DynamicOnlyKey<Definitions>,
+    Value extends Definitions[Key] extends DynamicConfigValueDefinition<
+      Key,
+      infer T
+    >
+      ? T
+      : never
+  >(
+    key: Key,
+    handler: (payload: { newValue: Value | undefined; oldValue: Value }) => void
+  ) => () => void
 }
 
 type DynamicOnlyKey<Definitions extends ExtendableOnlyDefinitions> = keyof {
-  [Key in StringKeys<Definitions> as Definitions[Key] extends DynamicConfigValueDefinition<any>
+  [Key in StringKeys<Definitions> as Definitions[Key] extends DynamicConfigValueDefinition<
+    Key & string,
+    any
+  >
     ? Key
     : never]: Definitions[Key]
 }
 
 type StaticOnlyKey<Definitions extends ExtendableOnlyDefinitions> = keyof {
-  [Key in StringKeys<Definitions> as Definitions[Key] extends StaticConfigValueDefinition<any>
+  [Key in StringKeys<Definitions> as Definitions[Key] extends StaticConfigValueDefinition<
+    Key,
+    any
+  >
     ? Key
     : never]: Definitions[Key]
 }
 
-type ExtendableOnlyDefinitions = Readonly<
-  Record<string, ConfigValueDefinition<any>>
->
+type ExtendableOnlyDefinitions = {
+  [Key in string]: ConfigValueDefinition<Key, any>
+}
 
 export const createConfigValueRetriever = <
   Definitions extends ExtendableOnlyDefinitions
@@ -89,57 +100,50 @@ export const createConfigValueRetriever = <
     ) as Definitions
   })()
 
-  function configGet(key: StaticOnlyKey<Definitions>) {
-    const definition = _environmentDefinitions[
-      key
-    ] as StaticConfigValueDefinition<any>
-
-    return definition.value
-  }
-
-  async function configFetch<Key extends StringKeys<Definitions>>(
-    key: Key,
-    abortController: AbortController = new AbortController()
-  ) {
-    const definition = _environmentDefinitions[key]
-
-    if ('retrieve' in definition) {
-      const value = await definition.retrieve(key, abortController)
-      Object.assign(definition, { value })
-      _dispatch(key, 'value.change', value)
-      return value
-    }
-
-    return definition.value
-  }
-
-  type Listeners = Partial<
-    Record<
-      keyof Definitions,
-      Partial<
-        Record<
-          Action<Definitions>['type'],
-          Set<(payload: ReturnType<Action<Definitions>['register']>) => void>
-        >
-      >
+  const _onChangeNotifiers: {
+    [Key in keyof Definitions]?: Definitions[Key] extends DynamicConfigValueDefinition<
+      string,
+      infer T
     >
-  >
-  const _listeners: Listeners = {}
-
-  const _dispatch: Dispatch<Definitions> = (key, type, payload) =>
-    _listeners[key]?.[type]?.forEach((listener) => listener(payload))
+      ? Notifier<{ oldValue: T | undefined; newValue: T }>
+      : never
+  } = {}
 
   return {
-    get: configGet,
-    fetch: configFetch,
-    subscribe: (key, action, handler) => {
-      const valueListeners = (() => _listeners[key] ?? (_listeners[key] = {}))()
-      const actionListeners = (() =>
-        valueListeners[action] ?? (valueListeners[action] = new Set()))()
+    get: function (key: StaticOnlyKey<Definitions>) {
+      const definition = _environmentDefinitions[
+        key
+      ] as StaticConfigValueDefinition<string, any>
 
-      const _handler = handler as () => void
-      actionListeners.add(_handler)
-      return () => actionListeners.delete(_handler)
+      return definition.value
+    },
+    fetch: async function <Key extends StringKeys<Definitions>>(
+      key: Key,
+      abortController: AbortController = new AbortController()
+    ) {
+      const definition = _environmentDefinitions[key]
+
+      if ('retrieve' in definition) {
+        const value = await definition.retrieve(key, abortController)
+        const oldValue = 'value' in definition ? definition.value : undefined
+        Object.assign(definition, { value })
+
+        if (value !== oldValue) {
+          _onChangeNotifiers[key]?.notify({ newValue: value, oldValue })
+        }
+
+        return value
+      }
+
+      return definition.value
+    },
+    subscribe: {
+      onChange: (key, handler) => {
+        const notifier =
+          _onChangeNotifiers[key] ??
+          (_onChangeNotifiers[key] = createNotifier<any>())
+        return () => notifier?.subscribe(handler)
+      },
     },
   }
 }
