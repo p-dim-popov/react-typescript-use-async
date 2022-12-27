544e4a20975d0ea234d90631979831df1568be25
Peter Popov <p.dim.popov@gmail.com>
2022-12-27 22:33:46 +0200

    chore(test): update vitest, remove dom stuff for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 /// <reference types="vitest" />
-import {
-  defineConfig,
-  type UserConfigExport as DefaultUserConfigExport,
-} from 'vite'
-import type { UserConfigExport as VitestUserConfigExport } from 'vitest/config'
-import react from '@vitejs/plugin-react'
 
-type UserConfigExport = DefaultUserConfigExport & VitestUserConfigExport
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,13 +10,11 @@ export default defineConfig({
   },
   plugins: [react()],
   test: {
-    environment: 'happy-dom',
-    setupFiles: ['./__test__/test-setup.ts'],
     includeSource: ['src/**/*.{ts,tsx}'],
     coverage: {
-      reporter: ['text-summary', 'text'],
+      reporter: ['text-summary', 'text', 'json-summary', 'html'],
     },
     mockReset: true,
     restoreMocks: true,
   },
-} as UserConfigExport)
+})
