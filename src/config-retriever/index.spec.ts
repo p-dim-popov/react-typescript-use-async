33b757b21ba792524687406bbbf046a866b62814
Peter Popov <p.dim.popov@gmail.com>
2022-12-29 16:08:08 +0200

    feat(config-retriever): separate functions, add tests

--- src/config-retriever/index.spec.ts
+++ src/config-retriever/index.spec.ts
@@ -1,23 +1,153 @@
-import { createConfigValueRetriever } from './index'
+import {
+  createConfigValueRetriever,
+  fetchConfigValue,
+  getConfigValue,
+  subscribeOnValueChange,
+} from './index'
+import { describe, expect, it, vi } from 'vitest'
+import * as NotifierModule from '../notifier'
 
-const Config = createConfigValueRetriever(
-  {
-    KEY_1: {
-      value: [''],
+const fiddle = () => {
+  const Config = createConfigValueRetriever(
+    {
+      KEY_1: {
+        value: [''],
+      },
+      KEY_2: {
+        retrieve: async () => 'asdfd',
+        value: 123,
+      },
+      KEY_3: {
+        retrieve: async () => 'asdfd',
+      },
     },
-    KEY_2: {
-      retrieve: async () => 'asdfd',
-      value: 123,
-    },
-    KEY_3: {
-      retrieve: async () => 'asdfd',
-    },
-  },
-  {
-    dev: {},
-  }
-)
+    {
+      dev: {},
+    }
+  )
 
-const xxx = Config.get('KEY_1')
-Config.subscribe('KEY_1', 'value.change', (payload) => payload)
-console.log(xxx)
+  const xxx = Config.get('KEY_2')
+
+  console.log(xxx)
+}
+
+describe(getConfigValue.name, () => {
+  it('should return static config value', function () {
+    const get = getConfigValue({ key1: { value: 'test-value' } })
+
+    expect(get('key1')).toEqual('test-value')
+  })
+
+  it('should return undefined for dynamic config value', function () {
+    const get = getConfigValue({ key1: { retrieve: async () => 'test-value' } })
+
+    expect(get('key1')).toBeUndefined()
+  })
+
+  it('should return value for hybrid config value', function () {
+    const get = getConfigValue({
+      key1: { retrieve: async () => 'dynamic-value', value: 'static-value' },
+    })
+
+    expect(get('key1')).toEqual('static-value')
+  })
+})
+
+describe(fetchConfigValue.name, function () {
+  it('should return value from retriever', async function () {
+    const fetch = fetchConfigValue(
+      {
+        key1: { retrieve: async () => 'test-value' },
+      },
+      {}
+    )
+
+    expect(await fetch('key1')).toEqual('test-value')
+  })
+
+  it('should return return value for static configs', async function () {
+    const fetch = fetchConfigValue(
+      {
+        key1: { value: 'test-value' },
+      },
+      {}
+    )
+
+    expect(await fetch('key1')).toEqual('test-value')
+  })
+
+  it('should update static value after retrieving dynamic', async function () {
+    const definitions = {
+      key1: { value: 'static-value', retrieve: async () => 'dynamic-value' },
+    }
+
+    const fetch = fetchConfigValue(definitions, {})
+
+    await fetch('key1')
+
+    expect(definitions.key1.value).toEqual('dynamic-value')
+  })
+
+  it.each<[any, any]>([
+    [
+      { value: 'static-value', retrieve: async () => 'dynamic-value' },
+      { newValue: 'dynamic-value', oldValue: 'static-value' },
+    ],
+    [
+      { retrieve: async () => 'dynamic-value' },
+      { newValue: 'dynamic-value', oldValue: undefined },
+    ],
+  ])(
+    'should notify on updating dynamic value, %j, %j',
+    async function (definition, expectedPayload) {
+      const fetch = fetchConfigValue(
+        { key1: definition },
+        {
+          key1: notifierMock,
+        }
+      )
+
+      await fetch('key1')
+
+      expect(notifierMock.notify).toBeCalledWith(expectedPayload)
+    }
+  )
+})
+
+describe(subscribeOnValueChange.name, function () {
+  it('should create notifier if not existing', function () {
+    vi.spyOn(NotifierModule, 'createNotifier').mockReturnValue(notifierMock)
+
+    const notifiers = {}
+    subscribeOnValueChange(notifiers)('key1', vi.fn())
+
+    expect(notifiers).toHaveProperty('key1', notifierMock)
+  })
+
+  it('should add handler to notifier', function () {
+    const notifiers = { key1: notifierMock }
+    const handlerMock = vi.fn()
+    subscribeOnValueChange(notifiers)('key1', handlerMock)
+
+    expect(notifierMock.subscribe).toBeCalledWith(handlerMock)
+  })
+
+  it('should remove handler from notifier', function () {
+    const notifiers = {
+      key1: {
+        ...notifierMock,
+        subscribe: vi.fn().mockReturnValue(notifierMock.unsubscribe),
+      },
+    }
+    const handlerMock = vi.fn()
+    const unsubscribe = subscribeOnValueChange(notifiers)('key1', handlerMock)
+
+    expect(unsubscribe).toEqual(notifierMock.unsubscribe)
+  })
+})
+
+const notifierMock = {
+  notify: vi.fn(),
+  subscribe: vi.fn(),
+  unsubscribe: vi.fn(),
+}
--- src/config-retriever/index.ts
+++ src/config-retriever/index.ts
@@ -1,149 +1,158 @@
 import type { Notifier } from '../notifier'
 import { createNotifier } from '../notifier'
 
-type ConfigValueDefinition<K extends string, T> =
-  | StaticConfigValueDefinition<K, T>
-  | DynamicConfigValueDefinition<K, T>
-  | HybridConfigValueDefinition<K, T>
+type ConfigValueDefinition<T> =
+  | StaticConfigValueDefinition<T>
+  | DynamicConfigValueDefinition<T>
+  | HybridConfigValueDefinition<T>
 
-type BaseConfigValueDefinition<Key extends string> = {
-  key: Key
-}
-
-type StaticConfigValueDefinition<
-  K extends string,
-  T
-> = BaseConfigValueDefinition<K> & {
+type StaticConfigValueDefinition<T> = {
   value: T
 }
 
-type DynamicConfigValueDefinition<
-  K extends string,
-  T
-> = BaseConfigValueDefinition<K> & {
-  retrieve: (key: string, abortController: AbortController) => Promise<T>
+type DynamicConfigValueDefinition<T> = {
+  retrieve: (
+    key: string | number | symbol,
+    abortController: AbortController
+  ) => Promise<T>
 }
 
-type HybridConfigValueDefinition<
-  K extends string,
-  T
-> = StaticConfigValueDefinition<K, T> & DynamicConfigValueDefinition<K, T>
+type HybridConfigValueDefinition<T> = StaticConfigValueDefinition<T> &
+  DynamicConfigValueDefinition<T>
 
-type StringKeys<T> = keyof T & string
-
-type ConfigValueRetriever<Definitions extends ExtendableOnlyDefinitions> = {
-  get: <Key extends StaticOnlyKey<Definitions> & string>(
-    key: Key
-  ) => Definitions[Key] extends ConfigValueDefinition<Key, infer T> ? T : never
-  fetch: <Key extends StringKeys<Definitions>>(
-    key: Key,
+type ConfigValueRetriever<D extends Definitions<Dictionary>> = {
+  get: <K extends keyof D>(key: K) => ConfigGetValue<D, K>
+  fetch: <K extends keyof D>(
+    key: K,
     controller?: AbortController
-  ) => Promise<
-    Definitions[Key] extends ConfigValueDefinition<Key, infer T> ? T : never
-  >
-  subscribe: Subscribe<Definitions>
+  ) => Promise<AwaitedConfigFetchValue<D, K>>
+  subscribe: Subscribe<D>
 }
 
-type Subscribe<Definitions extends ExtendableOnlyDefinitions> = {
-  onChange: <
-    Key extends string & DynamicOnlyKey<Definitions>,
-    Value extends Definitions[Key] extends DynamicConfigValueDefinition<
-      Key,
-      infer T
-    >
-      ? T
-      : never
-  >(
-    key: Key,
-    handler: (payload: { newValue: Value | undefined; oldValue: Value }) => void
+type Subscribe<D extends Definitions<Dictionary>> = {
+  onChange: <K extends keyof D>(
+    key: K,
+    handler: (
+      payload: ChangedValueEventPayload<ConfigDefinitionValue<D[K]>>
+    ) => void
   ) => () => void
 }
 
-type DynamicOnlyKey<Definitions extends ExtendableOnlyDefinitions> = keyof {
-  [Key in StringKeys<Definitions> as Definitions[Key] extends DynamicConfigValueDefinition<
-    Key & string,
-    any
-  >
-    ? Key
-    : never]: Definitions[Key]
-}
+type Dictionary = Record<string, unknown>
 
-type StaticOnlyKey<Definitions extends ExtendableOnlyDefinitions> = keyof {
-  [Key in StringKeys<Definitions> as Definitions[Key] extends StaticConfigValueDefinition<
-    Key,
-    any
-  >
-    ? Key
-    : never]: Definitions[Key]
-}
-
-type ExtendableOnlyDefinitions = {
-  [Key in string]: ConfigValueDefinition<Key, any>
+type Definitions<T extends Dictionary> = {
+  [K in keyof T]: ConfigValueDefinition<T[K]>
 }
 
 export const createConfigValueRetriever = <
-  Definitions extends ExtendableOnlyDefinitions
+  T extends Dictionary,
+  D extends Definitions<T>
 >(
-  definitions: Definitions,
-  environmentOverrides: Record<
-    string,
-    Partial<{ [Key in StringKeys<Definitions>]: Definitions[Key] }>
-  >
-): ConfigValueRetriever<Definitions> => {
-  const _environmentDefinitions: Definitions = (() => {
+  definitions: D,
+  environmentOverrides: Record<string, Definitions<Partial<T>>>
+): ConfigValueRetriever<D> => {
+  const _environmentDefinitions: D = (() => {
     const env = 'development'
     return Object.fromEntries(
       Object.entries(definitions).map(([key, value]) => [
         key,
         { ...value, ...environmentOverrides[env][key] },
       ])
-    ) as Definitions
+    ) as D
   })()
 
-  const _onChangeNotifiers: {
-    [Key in keyof Definitions]?: Definitions[Key] extends DynamicConfigValueDefinition<
-      string,
-      infer T
-    >
-      ? Notifier<{ oldValue: T | undefined; newValue: T }>
-      : never
-  } = {}
+  const _onChangeNotifiers: ChangedValueNotifiers<D> = {}
 
   return {
-    get: function (key: StaticOnlyKey<Definitions>) {
-      const definition = _environmentDefinitions[
-        key
-      ] as StaticConfigValueDefinition<string, any>
-
-      return definition.value
-    },
-    fetch: async function <Key extends StringKeys<Definitions>>(
-      key: Key,
-      abortController: AbortController = new AbortController()
-    ) {
-      const definition = _environmentDefinitions[key]
-
-      if ('retrieve' in definition) {
-        const value = await definition.retrieve(key, abortController)
-        const oldValue = 'value' in definition ? definition.value : undefined
-        Object.assign(definition, { value })
-
-        if (value !== oldValue) {
-          _onChangeNotifiers[key]?.notify({ newValue: value, oldValue })
-        }
-
-        return value
-      }
-
-      return definition.value
-    },
+    get: getConfigValue(_environmentDefinitions),
+    fetch: fetchConfigValue(_environmentDefinitions, _onChangeNotifiers),
     subscribe: {
-      onChange: (key, handler) => {
-        const notifier =
-          _onChangeNotifiers[key] ??
-          (_onChangeNotifiers[key] = createNotifier<any>())
-        return () => notifier?.subscribe(handler)
-      },
+      onChange: subscribeOnValueChange(_onChangeNotifiers),
     },
   }
 }
+
+type ChangedValueEventPayload<T> = { oldValue: T | undefined; newValue: T }
+
+type ChangedValueNotifiers<D extends Definitions<Dictionary>> = {
+  [K in keyof D]?: Notifier<
+    ChangedValueEventPayload<ConfigDefinitionValue<D[K]>>
+  >
+}
+
+export const getConfigValue =
+  <D extends Definitions<Dictionary>>(definitions: D) =>
+  <K extends keyof D>(key: K): ConfigGetValue<D, K> => {
+    const definition = definitions[key]
+
+    if ('value' in definition) {
+      return definition.value as ConfigGetValue<D, K>
+    }
+
+    return undefined as ConfigGetValue<D, K>
+  }
+
+type ConfigGetValue<
+  D extends Definitions<Dictionary>,
+  K extends keyof D
+> = D[K] extends StaticConfigValueDefinition<infer S>
+  ? S
+  : D[K] extends DynamicConfigValueDefinition<infer D>
+  ? D | undefined
+  : never
+
+export const fetchConfigValue =
+  <D extends Definitions<Dictionary>>(
+    definitions: D,
+    notifiers: ChangedValueNotifiers<D>
+  ) =>
+  async <K extends keyof D>(
+    key: K,
+    abortController = new AbortController()
+  ): Promise<AwaitedConfigFetchValue<D, K>> => {
+    const definition = definitions[key]
+
+    if ('retrieve' in definition) {
+      const value = (await definition.retrieve(
+        key,
+        abortController
+      )) as AwaitedConfigFetchValue<D, K>
+
+      const oldValue = (
+        'value' in definition ? definition.value : undefined
+      ) as AwaitedConfigFetchValue<D, K>
+
+      Object.assign(definition, { value })
+
+      if (value !== oldValue) {
+        notifiers[key]?.notify({ newValue: value, oldValue })
+      }
+
+      return value
+    }
+
+    return definition.value as AwaitedConfigFetchValue<D, K>
+  }
+
+type AwaitedConfigFetchValue<
+  D extends Definitions<Dictionary>,
+  K extends keyof D
+> = ConfigDefinitionValue<D[K]>
+
+export const subscribeOnValueChange =
+  <D extends Definitions<Dictionary>>(notifiers: ChangedValueNotifiers<D>) =>
+  <K extends keyof D>(
+    key: K,
+    handler: (
+      payload: ChangedValueEventPayload<ConfigDefinitionValue<D[K]>>
+    ) => void
+  ) => {
+    const notifier =
+      notifiers[key] ??
+      (notifiers[key] =
+        createNotifier<ChangedValueEventPayload<ConfigDefinitionValue<D[K]>>>())
+    return notifier?.subscribe(handler)
+  }
+
+type ConfigDefinitionValue<D extends ConfigValueDefinition<any>> =
+  D extends ConfigValueDefinition<infer O> ? O : never
